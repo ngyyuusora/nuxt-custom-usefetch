1c52a280826172c53e759a9a67497ab43cd689dd
ngyyuusora <jiliangsoft@live.com>
2025-04-21 22:02:41 +0800

    fix

--- app/types/auto-imports.d.ts
+++ app/types/auto-imports.d.ts
@@ -7,26 +7,39 @@
 export {}
 declare global {
   const ApolloError: (typeof import('@apollo/client/core'))['ApolloError']
+  const ContentTypeEnum: typeof import('../utils/request/types')['ContentTypeEnum']
   const EffectScope: (typeof import('vue'))['EffectScope']
   const NetworkStatus: (typeof import('@apollo/client/core'))['NetworkStatus']
+  const VUseFetch: typeof import('../utils/request/VUseFetch')['VUseFetch']
   const acceptHMRUpdate: typeof import('pinia')['acceptHMRUpdate']
+  const buildShortUUID: typeof import('../utils/uuid')['buildShortUUID']
+  const buildUUID: typeof import('../utils/uuid')['buildUUID']
   const computed: (typeof import('vue'))['computed']
   const createApp: (typeof import('vue'))['createApp']
+  const createFetch: typeof import('../utils/request/index')['createFetch']
   const createPinia: typeof import('pinia')['createPinia']
   const customRef: (typeof import('vue'))['customRef']
+  const dateUtil: typeof import('../utils/dateUtil')['dateUtil']
   const defineAsyncComponent: (typeof import('vue'))['defineAsyncComponent']
   const defineComponent: (typeof import('vue'))['defineComponent']
   const defineStore: typeof import('pinia')['defineStore']
   const effectScope: (typeof import('vue'))['effectScope']
+  const formatDateTimeIntl: typeof import('../utils/dateUtil')['formatDateTimeIntl']
+  const formatMiliSec2mmss: typeof import('../utils/dateUtil')['formatMiliSec2mmss']
+  const formatRequestDate: typeof import('../utils/request/util')['formatRequestDate']
+  const formatToDate: typeof import('../utils/dateUtil')['formatToDate']
+  const formatToDateTime: typeof import('../utils/dateUtil')['formatToDateTime']
   const getActivePinia: typeof import('pinia')['getActivePinia']
   const getCurrentInstance: (typeof import('vue'))['getCurrentInstance']
   const getCurrentScope: (typeof import('vue'))['getCurrentScope']
   const h: (typeof import('vue'))['h']
   const inject: (typeof import('vue'))['inject']
+  const interceptors: typeof import('../utils/request/index')['interceptors']
   const isProxy: (typeof import('vue'))['isProxy']
   const isReactive: (typeof import('vue'))['isReactive']
   const isReadonly: (typeof import('vue'))['isReadonly']
   const isRef: (typeof import('vue'))['isRef']
+  const joinTimestamp: typeof import('../utils/request/util')['joinTimestamp']
   const mapActions: typeof import('pinia')['mapActions']
   const mapGetters: typeof import('pinia')['mapGetters']
   const mapState: typeof import('pinia')['mapState']
@@ -76,9 +89,11 @@ declare global {
   const reactive: (typeof import('vue'))['reactive']
   const readonly: (typeof import('vue'))['readonly']
   const ref: (typeof import('vue'))['ref']
+  const requestPublic: typeof import('../utils/request/index')['requestPublic']
   const resolveComponent: (typeof import('vue'))['resolveComponent']
   const setActivePinia: typeof import('pinia')['setActivePinia']
   const setMapStoreSuffix: typeof import('pinia')['setMapStoreSuffix']
+  const setObjToUrlParams: typeof import('../utils/request/util')['setObjToUrlParams']
   const shallowReactive: (typeof import('vue'))['shallowReactive']
   const shallowReadonly: (typeof import('vue'))['shallowReadonly']
   const shallowRef: (typeof import('vue'))['shallowRef']
@@ -103,23 +118,47 @@ declare global {
   const watchPostEffect: (typeof import('vue'))['watchPostEffect']
   const watchSyncEffect: (typeof import('vue'))['watchSyncEffect']
 }
+// for type re-export
+declare global {
+  // @ts-ignore
+  export type { VUseFetch } from '../utils/request/VUseFetch'
+  import('../utils/request/VUseFetch')
+  // @ts-ignore
+  export type { ContentTypeEnum, RequestOptions, ApiResponse, CreateFetchOptions } from '../utils/request/types'
+  import('../utils/request/types')
+}
 
 // for vue template auto import
 import { UnwrapRef } from 'vue'
 declare module 'vue' {
   interface GlobalComponents {}
   interface ComponentCustomProperties {
+    readonly ContentTypeEnum: UnwrapRef<typeof import('../utils/request/types')['ContentTypeEnum']>
+    readonly VUseFetch: UnwrapRef<typeof import('../utils/request/VUseFetch')['VUseFetch']>
     readonly acceptHMRUpdate: UnwrapRef<typeof import('pinia')['acceptHMRUpdate']>
+    readonly buildShortUUID: UnwrapRef<typeof import('../utils/uuid')['buildShortUUID']>
+    readonly buildUUID: UnwrapRef<typeof import('../utils/uuid')['buildUUID']>
+    readonly createFetch: UnwrapRef<typeof import('../utils/request/index')['createFetch']>
     readonly createPinia: UnwrapRef<typeof import('pinia')['createPinia']>
+    readonly dateUtil: UnwrapRef<typeof import('../utils/dateUtil')['dateUtil']>
     readonly defineStore: UnwrapRef<typeof import('pinia')['defineStore']>
+    readonly formatDateTimeIntl: UnwrapRef<typeof import('../utils/dateUtil')['formatDateTimeIntl']>
+    readonly formatMiliSec2mmss: UnwrapRef<typeof import('../utils/dateUtil')['formatMiliSec2mmss']>
+    readonly formatRequestDate: UnwrapRef<typeof import('../utils/request/util')['formatRequestDate']>
+    readonly formatToDate: UnwrapRef<typeof import('../utils/dateUtil')['formatToDate']>
+    readonly formatToDateTime: UnwrapRef<typeof import('../utils/dateUtil')['formatToDateTime']>
     readonly getActivePinia: UnwrapRef<typeof import('pinia')['getActivePinia']>
+    readonly interceptors: UnwrapRef<typeof import('../utils/request/index')['interceptors']>
+    readonly joinTimestamp: UnwrapRef<typeof import('../utils/request/util')['joinTimestamp']>
     readonly mapActions: UnwrapRef<typeof import('pinia')['mapActions']>
     readonly mapGetters: UnwrapRef<typeof import('pinia')['mapGetters']>
     readonly mapState: UnwrapRef<typeof import('pinia')['mapState']>
     readonly mapStores: UnwrapRef<typeof import('pinia')['mapStores']>
     readonly mapWritableState: UnwrapRef<typeof import('pinia')['mapWritableState']>
+    readonly requestPublic: UnwrapRef<typeof import('../utils/request/index')['requestPublic']>
     readonly setActivePinia: UnwrapRef<typeof import('pinia')['setActivePinia']>
     readonly setMapStoreSuffix: UnwrapRef<typeof import('pinia')['setMapStoreSuffix']>
+    readonly setObjToUrlParams: UnwrapRef<typeof import('../utils/request/util')['setObjToUrlParams']>
     readonly storeToRefs: UnwrapRef<typeof import('pinia')['storeToRefs']>
     readonly useThemeStore: UnwrapRef<typeof import('../hooks/useTheme')['useThemeStore']>
   }
