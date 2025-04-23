46152dc32fe64d4765d3731d2916bae7a458c97d
ngyyuusora <jiliangsoft@live.com>
2025-04-23 15:56:45 +0800

    fix(util): formatToDateTime arg type

--- app/utils/dateUtil.ts
+++ app/utils/dateUtil.ts
@@ -4,7 +4,7 @@ const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 const DATE_FORMAT = 'YYYY-MM-DD';
 
 export function formatToDateTime(
-  date: dayjs.Dayjs | undefined = undefined,
+  date: string | number | dayjs.Dayjs | undefined = undefined,
   format = DATE_TIME_FORMAT,
 ): string {
   return dayjs(date).format(format);
